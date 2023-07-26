091e2eab57c855ad686d1ff707699f40a63b81b0
Igor Zupunski <ipatheword@live.com>
2023-07-26 14:40:54 +0200

    re done some funcs

--- src/pages/task-list-view/TaskListView.js
+++ src/pages/task-list-view/TaskListView.js
@@ -1,47 +1,35 @@
 import React, { useState } from "react";
 
 export default function TaskListView() {
-    const initialTasks = [
-        {
-            id: 1, title: "Task 1", isComplete: false,
-            id: 2, title: "Task 2", isComplete: true,
-            id: 3, title: "Task 3", isComplete: false,
-        }
-    ];
-
-    const [tasks, setTasks] = useState(initialTasks);
+    const [tasks, setTasks] = useState([]);
+    const [newTaskTitle, setNewTaskTitle] = useState("");
 
     const handleTaskCheckboxChange = (taskId) => {
-        setTasks((prevTasks) =>
-        prevTasks.map((task) => 
-        task.id === taskId ? { ...task, isComplete: !task.isComplete} : task
+        setTasks((prevTasks) => 
+            prevTasks.map((task) =>
+                task.id === taskId ? { ...task, isComplete: !task.isComplete } : task
             )
         );
     };
 
+    const handleAddTask = () => {
+        if (newTaskTitle.trim() === "") {
+            return;
+        }
+
+        const newTask = {
+            id: Date.now(),
+            title: newTaskTitle,
+            isComplete: false,
+        };
+
+        setTasks((prevTasks) => [ ...prevTasks, newTask ]);
+        setNewTaskTitle("");
+    };
+
     return(
         <div>
-            <h1>
-                Task Manager
-            </h1>
-            <ul>
-                {tasks.map((task) => (
-                    <li key={task.id}>
-                        <input 
-                            type="checkbox"
-                            checked={task.isComplete}
-                            onChange={() => handleTaskCheckboxChange(task.id)}
-                        />
-                        <span
-                            style={{
-                                textDecoration: task.isComplete ? "line-through" : "none"
-                            }}
-                        >
-                            {task.title}
-                        </span>
-                    </li>
-                ))}
-            </ul>
+
         </div>
     );
 }
